c12eec353749dc8d71a7aaf001fb9e8946786c3e
junerver <junerver@gmail.com>
2015-11-28 23:59:33 +0800

    完善了后退图标的交互

--- app/src/main/java/com/junerver/testo2oapp/activity/NewIndentActivity.java
+++ app/src/main/java/com/junerver/testo2oapp/activity/NewIndentActivity.java
@@ -6,26 +6,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.junerver.testo2oapp.R;
 import com.junerver.testo2oapp.utils.BaseActivity;
 
-public class NewIndentActivity extends BaseActivity {
+public class NewIndentActivity extends BaseActivity  implements View.OnClickListener {
     private ImageView img_back;
+    private FrameLayout framelayout_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_indent);
 
+        initView();
+
+    }
+
+    private void initView() {
         img_back = (ImageView) findViewById(R.id.img_back);
-        img_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        framelayout_back = (FrameLayout) findViewById(R.id.framelayout_back);
+        img_back.setOnClickListener(this);
+        framelayout_back.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +52,18 @@ public class NewIndentActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.framelayout_back:
+            case R.id.img_back:
+                finish();
+                break;
+            default:
+                break;
+        }
+
     }
 }
